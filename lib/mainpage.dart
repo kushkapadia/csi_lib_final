8595caf6e34817843472931daffa332f321e7119
siro844 <140879194+siro844@users.noreply.github.com>
2023-10-28 16:22:51 +0530

    misc

--- lib/mainpage.dart
+++ lib/mainpage.dart
@@ -24,7 +24,7 @@ class _MainPageState extends State<MainPage> {
     const ProductCard(
         image:
             'https://mir-s3-cdn-cf.behance.net/project_modules/1400/9d22a671392953.5bc4494680bce.jpg',
-        genre: 'Artiyguyig',
+        genre: 'Art',
         title: 'Gestalt'),
     const ProductCard(
         image:
